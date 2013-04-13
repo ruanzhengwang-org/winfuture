e2082c8935962073ef057de7e57d523622420a46
Zhengwang Ruan <ruan.zhengwang@gmail.com>
2013-04-13 14:37:39 +0800

    WinFuture: Basic code and framework.
    Add basic code to trade and mySql database support.
    
    Signed-off-by: Zhengwang Ruan <ruan.zhengwang@gmail.com>

---
+++ data.py
@@ -0,0 +1,53 @@
+#! /usr/bin/python
+
+import mysqldb as sql
+
+class Data:
+	def __init__ (self, database):
+		self.db = sql.MYSQL("localhost", "root", "19851117", database)
+		self.db.connect()
+		return
+	
+	def __exit__ (self):
+		self.db.close()
+		return 
+	
+	def sum (self, data):
+		i = 0
+		sum = 0
+		#print data[0], data[0][0], len(data)
+
+		while i < len(data):
+			sum = sum + data[i][0]
+			i = i + 1
+			
+		return sum
+
+	def avg (self, data, count):
+		return self.sum(data) / count
+	
+	def M (self, table, date, filed='Close', days=1):
+		cond = 'Time <= "%s" order by Time desc limit %d' % (date, days)
+		#print cond
+		num = self.db.search(table, cond, filed)
+		res = self.db.fetch('all')
+		#print res
+		#print res[0][0]
+
+		return self.avg(res, num)
+	
+	def M5 (self, table, date, filed='Close'):
+		return self.M(table, date, filed, 5)
+	
+	def M10 (self, table, date, filed='Close'):
+		return self.M(table, date, filed, 10)
+	
+	def M20 (self, table, date, filed='Close'):
+		return self.M(table, date, filed, 20)
+	
+	def M40 (self, table, date, filed='Close'):
+		return self.M(table, date, filed, 40)
+	
+	def M60 (self, table, date, filed='Close'):
+		return self.M(table, date, filed, 60)
+	
---
+++ date.py
@@ -0,0 +1,57 @@
+#!/usr/bin/python
+
+import mysqldb as sql
+
+class Date:
+	def __init__ (self, database, table):
+		self.db = sql.MYSQL("localhost", "root", "19851117", database)
+		self.db.connect()
+		self.fillDates(table)
+		return
+	
+	def __exit__ (self):
+		self.db.close()
+		return
+	
+	def fillDates (self, table):
+		sqls = 'select Time from %s order by %s asc' % (table, 'Time')
+		#self.db.search(table, None, 'Day')
+		self.db.execSql(sqls)
+		self.dateSet = self.db.fetch('all')
+		self.dateIndex = 0
+		self.indexBound = len(self.dateSet)
+		#print self.indexBound
+		#print self.dateSet
+		return
+	
+	def curDate (self):
+		if self.dateIndex >= 0 and self.dateIndex < self.indexBound:
+			return self.dateSet[self.dateIndex][0]
+		return None
+	
+	def nextDate (self):
+		if self.dateIndex + 1 < self.indexBound:
+			return self.dateSet[self.dateIndex + 1][0]
+		return None
+		
+	def setCurDate (self, day):
+		#print day
+		#time = '%s' % (self.dateSet[0][0])
+		#print time
+		i = 0
+		while i < self.indexBound:
+			time = '%s' % (self.dateSet[i][0])
+			if time == day:
+				self.dateIndex = i
+				return i
+			i = i + 1
+			
+		return None
+	
+	def getSetNextDate (self):
+		if self.dateIndex + 1 < self.indexBound:
+			self.dateIndex = self.dateIndex + 1
+			return self.dateSet[self.dateIndex][0]
+		else:
+			return None
+	
