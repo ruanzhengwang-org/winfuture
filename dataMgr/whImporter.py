bb7727443189bc088c6c3de988bf2d185865bdde
Zhengwang Ruan <ruan.zhengwang@gmail.com>
2013-04-25 23:56:10 +0800

    dataMgr:whImporter: add processRawRecords method
    1) Add processRawRecords() which processes raw records before
       calling importFromDir().
    2) Correct and remove commentary.
    
    Signed-off-by: Zhengwang Ruan <ruan.zhengwang@gmail.com>

--- dataMgr/whImporter.py
+++ dataMgr/whImporter.py
@@ -6,7 +6,6 @@ import importer as IMPORT
 
 class WenhuaImport(IMPORT.Import):
 	def __init__ (self, database='futures'):
-		#IMPORT.Import.__init__(self, dataFile, dataTable, database)
 		IMPORT.Import.__init__(self, database)
 		return
 	
@@ -14,7 +13,6 @@ class WenhuaImport(IMPORT.Import):
 		IMPORT.Import.__exit__(self)
 		return
 	
-	#def wenhuaImport (self, tFrom, tTo):
 	def newImport (self, dataFile, dataTable):
 		self.prepareImport(dataTable)
 		
@@ -37,7 +35,7 @@ class WenhuaImport(IMPORT.Import):
 	def getRecordData(self, record):
 		return self.getRecordField(record, 2)
 	
-	# Import data records from a directory to database.
+	# Import data records in a directory to database.
 	def importFromDir (self, directory, dataTable):
 		self.prepareImport(dataTable)
 		
@@ -73,5 +71,23 @@ class WenhuaImport(IMPORT.Import):
 		hFile.close()
 		lFile.close()
 		cFile.close()
+		
+	# processRawRecords should ONLY be used combined with importFromDir(), 
+	# which aims to remove the uesless header and descriptions on the top 
+	# of data records imported from WenHua.
+	def processRawRecords(self, directory, lines=43):
+		oFile = '%s/o.txt' % (directory)
+		hFile = '%s/h.txt' % (directory)
+		lFile = '%s/l.txt' % (directory)
+		cFile = '%s/c.txt' % (directory)
+		
+		cmdStr = 'sed -i "1,%sd" %s' % (lines, oFile)
+		os.popen(cmdStr)
+		cmdStr = 'sed -i "1,%sd" %s' % (lines, hFile)
+		os.popen(cmdStr)
+		cmdStr = 'sed -i "1,%sd" %s' % (lines, lFile)
+		os.popen(cmdStr)
+		cmdStr = 'sed -i "1,%sd" %s' % (lines, cFile)
+		os.popen(cmdStr)
 		
 	
